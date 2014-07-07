d03cfb7b0516cb4f4b2829fc28cb2ea94d5175d6
Megan Parkes <megan@theparkes.me.uk>
2014-07-07 16:43:40 +0100

    added rest of items

---
+++ jethtml/src/comicbook/Publishers.java
@@ -0,0 +1,42 @@
+package comicbook;
+
+import comicBooks.*;
+import comicBooks.impl.*;
+
+public class Publishers
+{
+  protected static String nl;
+  public static synchronized Publishers create(String lineSeparator)
+  {
+    nl = lineSeparator;
+    Publishers result = new Publishers();
+    nl = null;
+    return result;
+  }
+
+  public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+  protected final String TEXT_1 = "";
+  protected final String TEXT_2 = NL + "<h2>Publishers</h2>" + NL + "<ul>";
+  protected final String TEXT_3 = NL + "\t<li><a href=\"";
+  protected final String TEXT_4 = ".html\">";
+  protected final String TEXT_5 = "</a><br></li>";
+  protected final String TEXT_6 = NL + "</ul>" + NL + "" + NL + "<a href=\"index.html\">Back</a>";
+
+  public String generate(Object argument)
+  {
+    final StringBuffer stringBuffer = new StringBuffer();
+    stringBuffer.append(TEXT_1);
+     ComicBooksFactoryImpl cbcf = new ComicBooksFactoryImpl(); 
+     ComicBookCollection comicBookCollection = cbcf.createComicBookCollection(); 
+    stringBuffer.append(TEXT_2);
+     for (Publisher publisher : comicBookCollection.getPublishingCompanies()) { 
+    stringBuffer.append(TEXT_3);
+    stringBuffer.append(publisher.getPublishersName());
+    stringBuffer.append(TEXT_4);
+    stringBuffer.append(publisher.getPublishersName());
+    stringBuffer.append(TEXT_5);
+     } 
+    stringBuffer.append(TEXT_6);
+    return stringBuffer.toString();
+  }
+}
--- jethtml/src/test/Test.java
+++ jethtml/src/test/Test.java
@@ -11,5 +11,25 @@ public class Test {
 	 Artists artists = new Artists();
 	 String resultTwo = artists.generate(null);
 	 System.out.println(resultTwo);
+	 
+	 Editors editors = new Editors();
+	 String resultThree = editors.generate(null);
+	 System.out.println(resultThree);
+	 
+	 Writers writers = new Writers();
+	 String resultFour = writers.generate(null);
+	 System.out.println(resultFour);
+	 
+	 Publishers publishers = new Publishers();
+	 String resultFive = publishers.generate(null);
+	 System.out.println(resultFive);
+	 
+	 Series series= new Series();
+	 String resultSix = series.generate(null);
+	 System.out.println(resultSix);
+	 
+	 Books books= new Books();
+	 String resultSeven = books.generate(null);
+	 System.out.println(resultSeven);
 	}
 }
