448f543736ecea62c7b458a95556fba65c40c82a
Megan Parkes <mep513@york.ac.uk>
2014-08-30 15:31:26 +0100

    JetTest Works

--- Jet2Egl/JetTest.java
+++ Jet2Egl/JetTest.java
@@ -1,4 +1,3 @@
-package test;
 import comicbook.*;
 
 import java.io.File;
@@ -8,17 +7,16 @@ import org.eclipse.epsilon.eol.models.IModel;
 
 import comicBooks.ComicBooksPackage;
 
-public class TestJet{
+public class JetTest{
 
 	public static void main(String[] args) throws Exception {
 		Artists2 artists2 = new Artists2();
 		
-		File modelFile = new File("usr/userfs/m/mep513/Documents/EGLStuff/Jet2Egl/tmp/comicBooks.model");
+		File modelFile = new File("./tmp/comicBooks.model");
 		IModel model = EmfModelFactory.getInstance().loadEmfModel("OurModel", modelFile, ComicBooksPackage.eINSTANCE);
 
 		
 		for (Object component : model.getAllOfKind("Artist")) {
-		    System.out.println("hello");
 			System.out.println(artists2.generate(component));
 		}
 	}
---
+++ Jet2Egl/readme.txt
@@ -0,0 +1,25 @@
+README
+
+To run:
+
+run: sh test.sh
+
+
+
+
+
+commands for me
+
+compile TestJet javac -cp packages/*:src:. src/test/TestJet.java
+
+
+
+java -cp 
+
+epsilon-1.1_SR1-ant-emf.jar:
+org.eclipse.emf.ecore_2.9.0.v20130528-0742.jar:
+org.eclipse.emf.common_2.9.0.v20130528-0742.jar:
+org.eclipse.emf.ecore.xmi_2.9.0.v20130528-0742.jar:
+. Testing
+
+
