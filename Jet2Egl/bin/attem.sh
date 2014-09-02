a48adfaf17c133b78ba480676b3a2a918e5ad93a
Megan Parkes <mep513@york.ac.uk>
2014-09-02 13:46:16 +0100

    imports for testing broken

---
+++ Jet2Egl/bin/attem.sh
@@ -0,0 +1,6 @@
+echo "Testing"
+echo "Compiling"
+javac -cp packages/*:src:. TempJetTest.java
+echo "Running"
+java -cp packages/*:src:. TempJetTest
+
--- Jet2Egl/testWriter.py
+++ Jet2Egl/testWriter.py
@@ -8,8 +8,8 @@ def writeTests(name, src, directory_of_files_translating, model):
     imports = set()
     classList = set()
     test_file= open(name+".java", "w")
-    
-    
+    # This is added onto each var name incase they are a keyword (eg. class)
+    addition = "temp"
     #Write imports
     test_file.write("import org.eclipse.epsilon.emc.emf.EmfModelFactory;\nimport org.eclipse.epsilon.eol.models.IModel;\nimport java.io.File;\nimport java.io.PrintWriter;\n")
     
@@ -20,10 +20,22 @@ def writeTests(name, src, directory_of_files_translating, model):
     
     for i in imports:
         test_file.write("import "+ i + ".*;\n")
-        
-    
+       
+    imports = set()
+    # import all classes
+    fmatches = []
+    for root, dirnames, files in os.walk(src):
+        for fname in files:
+            if fname.endswith(".java"):
+                imports = addImportClass(os.path.join(root, fname), imports, src)
+            
+    for i in imports:
+        test_file.write("import "+ i + ";\n")
+
+       
+    #test_file.write("import emf.*;")
     #Write main class
-    test_file.write("\npublic class JetTest{\n\tpublic static void main(String[] args) throws Exception {\n")
+    test_file.write("\npublic class " +name+"{\n\tpublic static void main(String[] args) throws Exception {\n")
     
     #Write each class
     files = glob.glob(directory_of_files_translating)
@@ -31,19 +43,31 @@ def writeTests(name, src, directory_of_files_translating, model):
         temp = open(fname, "r")
         className = findClass(temp.readlines()[0])
         classList.add(className)
-        test_file.write("\t\t"+className.capitalize() + " " + className.lower() + " = new " + className.capitalize() + "();\n")
+        test_file.write("\t\t"+className.capitalize() + " " + className.lower()+addition + " = new " + className.capitalize() + "();\n")
         
     #Load Model
-    test_file.write("\t\tFile modelFile = new File(\"" + model +"\");\n\t\tIModel model = EmfModelFactory.getInstance().loadEmfModel(\"OurModel\", modelFile, ComicBooksPackage.eINSTANCE);\n\t\tPrintWriter writer = new PrintWriter(\"./compare/jet.txt\", \"UTF-8\");\n\t\tfor (Object component : model.allContents()){\n")
+    test_file.write("\t\tFile modelFile = new File(\"" + model +"\");\n\t\tIModel model = EmfModelFactory.getInstance().loadEmfModel(\"OurModel\", modelFile, ComicBooksPackage.eINSTANCE);\n\n")
     
     for i in classList:
-        test_file.write("\t\t\twriter.println(" + i.lower() +".generate(component));\n")
-    
-    test_file.write("\t\t}\n\t\twriter.close();\n\t}\n}")
+        test_file.write("\t\tPrintWriter writer"+i+" = new PrintWriter(\"./compare/jet"+i+".txt\", \"UTF-8\");\n\t\tfor (Object component : model.allContents()){\n")
+        test_file.write("\t\t\twriter"+i+".println(" + i.lower()+addition +".generate(component));\n\t\t}")
+        test_file.write("\n\t\twriter"+i+".close();\n\n")
+    test_file.write("\t}\n}")
      
 def addImport(directory, imports, src):
     if directory != src:
         imports.add(directory.split("/")[1])
+        return imports
+    else:
+        return imports
+        
+def addImportClass(name, imports, src):
+    for i in imports:
+        if name.split(".")[0].split("/")[-1] in i:
+            return imports
+    if name != src:
+        temp = src+"."
+        imports.add(name.split(".")[0].replace("/",".").replace(temp, ""))
         return imports
     else:
         return imports
@@ -57,5 +81,6 @@ def findClass(line):
 
 
 
+
 ## To run swap "./tmp/comicBooks.model" with your model     
-writeTests("attemptone", "src",  "jet/*", "./tmp/comicBooks.model" )
+writeTests("TempJetTest", "src",  "jet/*", "./tmp/comicBooks.model" )
